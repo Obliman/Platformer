67a8189796a255a297b7dde6681eda625403a16d
Obliman <nato5342@hotmail.com>
2015-04-23 22:28:35 -0500

    Added Anti-Gravity
    Hold M2 to instantly use one unit of charge plus one additional unit
    each 0.5 seconds the button is held to disable gravity on the player.
    Considering changing it to consuming all remaining charges when clicked
    and producing equivalent anti-gravity time.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,31 +3,15 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour {
 
-	public static int charges = 1;
+	public static int charges = 8;
     public static Vector3 position;
 	float chargeTime = 0.0f;
 	float genTime = 1.0f;
 	int maxCharges = 8;
-	//bool keyReleased = false;
     public Sprite[] img;
 
+
 	void FixedUpdate () {
-        /*
-		if (Input.GetKeyDown (KeyCode.W) && charges > 0 && keyReleased) {
-			transform.rigidbody2D.velocity = new Vector2 (transform.rigidbody2D.velocity.x, 5.0f);
-			charges--;
-			keyReleased = false;
-		}
-        if (Input.GetKeyUp (KeyCode.W)){
-            keyReleased = true;
-        }
-		if (Input.GetKey (KeyCode.A)) {
-			transform.rigidbody2D.velocity = new Vector2(-1.0f,transform.rigidbody2D.velocity.y);
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			transform.rigidbody2D.velocity = new Vector2(1.0f,transform.rigidbody2D.velocity.y);
-		}
-        */
         position = transform.position;
         GetComponent<SpriteRenderer>().sprite = img[charges];
 	}
--- Assets/Scripts/PlayerWave.cs
+++ Assets/Scripts/PlayerWave.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class PlayerWave : MonoBehaviour {
 
     public GameObject wave;
-	
+    public ParticleSystem part;
+    bool initPress = true;
+    float useRate = 0.5f;
+    float useTime = 0.0f;
+    	
 	void FixedUpdate () {
 	    if(Input.GetMouseButtonDown(0) && PlayerMove.charges > 0){
             Instantiate(wave,transform.position,Quaternion.identity);
@@ -13,6 +17,29 @@ public class PlayerWave : MonoBehaviour {
             var dir = Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
             //transform.rigidbody2D.velocity = -dir/Vector2.Distance(transform.position,Camera.main.ScreenToWorldPoint(Input.mousePosition))*5.0f;
             transform.rigidbody2D.AddForce(-dir/Vector2.Distance(transform.position,Camera.main.ScreenToWorldPoint(Input.mousePosition))*300.0f);
+        }
+
+        if(Input.GetMouseButton(1) && PlayerMove.charges > 0){
+            if(initPress){
+                PlayerMove.charges--;
+                initPress = false;
+            }
+            
+            useTime += Time.deltaTime;
+            
+            if(useTime <= useRate){
+                transform.rigidbody2D.gravityScale = 0.0f;
+                part.enableEmission = true;
+            }else{
+                transform.rigidbody2D.gravityScale = 1.0f;
+                useTime = 0.0f;
+                PlayerMove.charges--;
+            }
+        }else{
+            initPress = true;
+            transform.rigidbody2D.gravityScale = 1.0f;
+            part.enableEmission = false;
+            useTime = 0.0f;
         }
 	}
 }
